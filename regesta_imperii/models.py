69a49af2ce227f8bd22554e4453838534ef44a60
Fritz Otlinghaus <fritz@mealhub.de>
2015-04-26 08:57:32 +0200

    django init 2

---
+++ regesta_imperii/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+# Personen
+# -Name
+# -short_description
+# -birth_date
+# -death_date
+# Regeste
+# -Person
+# -Inhalt
+# -Datum
+# -Ort
+# Orte
+# Portrais
+#
